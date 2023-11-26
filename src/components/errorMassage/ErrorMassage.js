fbc257f32912c26292fa664906587d2d95ede226
Oleg <olegatortor111@gmail.com>
2023-11-26 14:25:51 +0200

    restart project

---
+++ src/components/errorMassage/ErrorMassage.js
@@ -0,0 +1,7 @@
+import img from './error.gif'
+
+const ErrorMassage = () => {
+    return <img src={img} style={{margin: '0 auto', height: 250}} alt="ERROR" />
+}
+
+export default ErrorMassage;
---
+++ src/services/MarvelServices.js
@@ -0,0 +1,36 @@
+class MarvelServices {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=';
+
+    getResurses = async (url) => {
+        let res = await fetch(url);
+
+        if(!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        }
+
+        return res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResurses(`${this._apiBase}characters?limit=9&offset=100&${this._apiKey}`)
+        return res.data.results.map(this._transformCharecter)
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResurses(`${this._apiBase}characters/${id}?${this._apiKey}`)
+        return this._transformCharecter(res.data.results[0])
+    }
+
+    _transformCharecter = (char) => {
+        return {
+            name: char.name,
+            descr: char.description ? `${char.description.slice(0, 215)}...`: 'Sorry, there is no description',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+}
+
+export default MarvelServices;
